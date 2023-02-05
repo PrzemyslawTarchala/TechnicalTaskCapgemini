13e9bf2d03f6254c6e6ad6a238d6771729d71603
Przemyslaw Tarchala <przemyslaw.tarchala.programista@gmail.com>
2023-02-05 16:52:23 +0100

    Add FIle cpp

---
+++ TechnicalTaskCapgemini/TechnicalTaskCapgemini/TechnicalTaskCapgemini.cpp
@@ -0,0 +1,13 @@
+﻿// TechnicalTaskCapgemini.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    return 0;
+}
+
